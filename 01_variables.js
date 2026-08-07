4989e894fd3a67ccd75d7b45ed365dcb851e7110
Axita <axitagohel12@gmail.com>
2026-08-07 12:04:18 +0530

    01_variables file comleted.

--- 01_variables.js
+++ 01_variables.js
@@ -2,3 +2,20 @@ const accountId = 1225
 let accountEmail = 'axitagohel12@gmail.com'
 var accountPassword = '12345'
 accountCity = 'Junagadh'
+let accountState;
+
+// accountId = 1226 //not allow to change its value.
+/* Prefer not to use var  */
+
+
+accountEmail = 'akgohel25@gmail.com'
+accountPassword = '67890'
+accountCity = 'Ahmedabad'
+
+/* console.log(accountId)
+console.log(accountEmail)
+console.log(accountPassword)
+console.log(accountCity)
+console.log(accontState)  */
+
+console.table({accountId, accountEmail, accountPassword, accountCity, accountState})
