3f10f69d38c6b2f0ce50dc6edc9c1aa1cc3132dc
Axita <axitagohel12@gmail.com>
2026-08-07 18:28:16 +0530

    04_comparision file completed.

---
+++ 04_comparision.js
@@ -0,0 +1,17 @@
+console.log(2 > 1)
+console.log(2 < 1)
+console.log(2 >= 1)
+console.log(2 == 1)
+console.log(2 != 1)
+
+console.log(2 == '2')
+console.log(2 === '2')  //strict equality operator
+console.log(2 !== '2')  //strict not equal operator
+
+console.log(null > 0)
+console.log(null == 0)
+console.log(null >= 0)
+
+console.log(undefined > 0)
+console.log(undefined == 0)
+console.log(undefined >= 0)
