7c735ddb9995f48becd1b334d4efc019f6945711
Axita <axitagohel12@gmail.com>
2026-08-07 14:30:25 +0530

    Add conversion Operation file.

---
+++ 03_conversionOperation.js
@@ -0,0 +1,22 @@
+let score = 25
+
+//console.log(typeof score) 
+//console.log(typeof (score))
+
+let value = "33"
+//console.log(typeof value)
+
+
+let value2 = "33abc"
+let value3 = Number(value2)
+
+//console.log(value3)     //NaN => Not a Number
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1 // " " // "Axita"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)  //true
