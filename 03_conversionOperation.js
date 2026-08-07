dd651209fb26a577a39042a80b470f7d9c256f27
Axita <axitagohel12@gmail.com>
2026-08-07 18:15:32 +0530

    03_conversionOpearion file completed.

--- 03_conversionOperation.js
+++ 03_conversionOperation.js
@@ -19,4 +19,33 @@ let value3 = Number(value2)
 
 let isLoggedIn = 1 // " " // "Axita"
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)  //true
+// console.log(booleanIsLoggedIn)  //true
+
+
+// *******************  Operations  *************************
+
+let simpleValue = 3
+let negValue = -simpleValue
+
+// console.log(negValue)  // -3
+
+let str1 = "Hello"
+let str2 = " World"
+
+let str3 = str1 + str2
+// console.log(str3)  // Hello World
+
+// console.log("2" + 3)
+// console.log(2 + "3")
+// console.log("2" + 3 + 3)
+// console.log(2 + 3 + "3")
+
+// console.log((2 + 3) * 5 / 4)
+
+// console.log(+true)
+// console.log(+false)
+// console.log(+"")
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter)
