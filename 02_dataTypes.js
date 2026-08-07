71df46f3338500cea85ac3bd4171067a19aa1c40
Axita <axitagohel12@gmail.com>
2026-08-07 14:04:26 +0530

    Add Datatype file.

---
+++ 02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict" // treat this js code as newer version of js by code engine.
+
+// alert(5 + 5)      // we are using nodejs code, not browser console.
+
+console.log('Axita')
+
+console.log(5 + 5)
+
+//number => 2 to power of 53 -1 => 9007199254740991
+// string => "Axita" or 'Axita' or `Axita`
+// boolean => true or false
+// null => empty value , standalone value
+// undefined => variable is declared but not assigned any value
+// symbol => unique value, used as object property key
+// object => collection of key value pairs, used to store data
+
+console.log(typeof 5)
+console.log(typeof 5.5)
+console.log(typeof age)
+console.log(typeof 'Axita')
+console.log(typeof true)
+console.log(typeof null)
+console.log(typeof undefined)
+console.log(typeof Symbol('id'))
+console.log(typeof {name: 'Axita', age: 22})
