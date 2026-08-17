5bdfa1fe4ee49f704a0bede8d4e3880674abe52f
Axita <axitagohel12@gmail.com>
2026-08-17 10:13:20 +0530

    Add Switch File.

---
+++ control_flow/switch.js
@@ -0,0 +1,39 @@
+const month = 3
+
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("February");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    default:
+        console.log("Dafault Case Match.");
+        break;
+}
+
+const month1 = "April"
+
+switch (month1) {
+    case "January":
+        console.log("January");
+        break;
+    case "February":
+        console.log("February");
+        break;
+    case "March":
+        console.log("March");
+        break;
+    case "April":
+        console.log("April");
+        break;
+    default:
+        console.log("Dafault Case Match.");
+        break;
+}
